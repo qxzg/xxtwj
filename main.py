28787a0dd970b9dc5cfce8f3fc3306042959d23b
qxzg <out@qxzg.xyz>
2023-04-12 17:23:58 +0800

    fix typo & update questionnaire

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from time import sleep
 from random import randint
 from os import system
 
-
 max_sleep = 30  # 两次请求间隔最长时间，越大耗时越久，但被检测到的风险越小
 min_sleep = 10  # 两次请求间隔最短时间
 
@@ -27,8 +26,9 @@ fid = ''
 uid = ''
 home_url = 'https://i.chaoxing.com/base'
 questionnaire_list_url = 'https://newes.chaoxing.com/newesReception/ajaxRatedHome'  # 从https://newes.chaoxing.com/newesReception/ratedHome页面获取的
-save_questionnaire_url = 'https://newes.chaoxing.com/newesReception/saveQuestionnaire'
+save_questionnaire_url = 'https://newes.chaoxing.com/pj/newesReception/saveQuestionnaire'
 questionnaire_remain = 0
+
 
 def check_cookie():
     req = BeautifulSoup(get(home_url, cookies=cookies, headers=headers).text, features='lxml')
@@ -43,6 +43,8 @@ def check_cookie():
             pass
         else:
             print("以 %s 的身份登陆成功。" % name.string)
+
+
 def raw_cookie_to_dist():
     global cookies, fid, uid
     cookie_list = cookie.split('; ')
@@ -54,27 +56,29 @@ def raw_cookie_to_dist():
     uid = cookies['_uid']
     check_cookie()
 
+
 def get_questionnaire_list():
     global questionnaire_list, questionnaire_remain
     req = get(
-        questionnaire_list_url + "?uId=%s&fid=%s&questionnaireType=4&courseInfo=&classificationId=&currentPage=1" % (uid, fid),
+        questionnaire_list_url + "?uId=%s&fid=%s&questionnaireType=4&courseInfo=&classificationId=&currentPage=1" % (
+        uid, fid),
         cookies=cookies, headers=headers
     ).json()
     page_count = req['pageCount']
     print("正在获取第 1/%s 页的问卷" % page_count)
-    for i in range(1, page_count):
-    # for i in range(1, 2):
+    for i in range(1, page_count + 1):
+        # for i in range(1, 2):
         if i != 1:  # i=1时无需重复请求
             sleep(randint(min_sleep, max_sleep))
             print("正在获取第 %s/%s 页的问卷" % (i, page_count))
             req = get(
-                questionnaire_list_url + "?uId=%s&fid=%s&questionnaireType=4&courseInfo=&classificationId=&currentPage=%d" % (uid, fid, i),
+                questionnaire_list_url + "?uId=%s&fid=%s&questionnaireType=4&courseInfo=&classificationId"
+                                         "=&currentPage=%d" % (uid, fid, i),
                 cookies=cookies, headers=headers
             ).json()
 
         questionnaire_count = req['html'].count('<tr>')
         soup = BeautifulSoup(req['html'], features='lxml').find_all('td')
-
         n = 0
         for j in range(questionnaire_count):
             questionnaire_data = {}
@@ -97,24 +101,23 @@ def get_questionnaire_list():
                 questionnaire_data['questionnaireId'] = link_info[2]
             questionnaire_list.append(questionnaire_data)
 
+
 def do_questionnaire(questionnaire_data: dict):
     payload = \
-    "uId=%s&fid=%s&questionnaireId=%s&alreadyId=%s&grantId=%s" % (uid, fid, questionnaire_data['questionnaireId'], questionnaire_data['alreadyId'], questionnaire_data['grantId']) +\
-    "&groupTargetIds=616111&616111_type=1&616111_chooseSetUp=-1&616111=2&groupTargetIds=616112&616112_type=1&\
-616112_chooseSetUp=-1&groupTargetIds=616113&616113_type=1&616113_chooseSetUp=-1&groupTargetIds=616114&616114_type=1&\
-616114_chooseSetUp=-1&groupTargetIds=616115&616115_type=1&616115_chooseSetUp=-1&groupTargetIds=616116&616116_type=1&\
-616116_chooseSetUp=-1&groupTargetIds=616117&616117_type=1&616117_chooseSetUp=-1&groupTargetIds=616118&616118_type=1&\
-616118_chooseSetUp=-1&groupTargetIds=616119&616119_type=1&616119_chooseSetUp=-1&groupTargetIds=616120&616120_type=1&\
-616120_chooseSetUp=-1&groupTargetIds=616121&616121_type=1&616121_chooseSetUp=-1&groupTargetIds=616122&616122_type=3&\
-616122_chooseSetUp=1&616122_fillNum=1&616122_1=0&groupTargetIds=616123&616123_type=3&616123_chooseSetUp=1&\
-616123_fillNum=1&616123_1=0&groupTargetIds=616124&616124_type=1&616124_chooseSetUp=-1&616124=1&\
-groupTargetIds=616125&616125_type=1&616125_chooseSetUp=-1&616125=1&groupTargetIds=616126&616126_type=1&\
-616126_chooseSetUp=-1&616126=2&groupTargetIds=616127&616127_type=1&616127_chooseSetUp=-1&616127=1&\
-groupTargetIds=616128&616128_type=1&616128_chooseSetUp=-1&616128=1&groupTargetIds=616129&616129_type=1&\
-616129_chooseSetUp=-1&616129=1&groupTargetIds=616130&616130_type=1&616130_chooseSetUp=-1&616130=1&\
-groupTargetIds=616131&616131_type=11&616131_chooseSetUp=1&616131=100&groupTargetIds=616132&616132_type=1&\
-616132_chooseSetUp=-1&616132=2&groupTargetIds=616133&616133_type=10&616133_chooseSetUp=1&\
-groupTargetIds=616134&616134_type=4&616134_chooseSetUp=1&jumpInfo=%7B&616134=%E6%97%A0&saveType=2&submitreasons="
+        "uId=%s&fid=%s&questionnaireId=%s&alreadyId=%s&grantId=%s" % \
+        (uid, fid, questionnaire_data['questionnaireId'], questionnaire_data['alreadyId'],
+         questionnaire_data['grantId']) + \
+        "&groupTargetIds=694391&694391_type=3&694391_chooseSetUp=1&694391_fillNum=1&694391_1=0&groupTargetIds=694392" \
+        "&694392_type=3&694392_chooseSetUp=1&694392_fillNum=1&694392_1=0&groupTargetIds=694393&694393_type=1" \
+        "&694393_chooseSetUp=-1&694393=1&groupTargetIds=694394&694394_type=1&694394_chooseSetUp=-1&694394=1" \
+        "&groupTargetIds=694395&694395_type=1&694395_chooseSetUp=-1&694395=1&groupTargetIds=694396&694396_type=1" \
+        "&694396_chooseSetUp=-1&694396=1&groupTargetIds=694397&694397_type=1&694397_chooseSetUp=-1&694397=1" \
+        "&groupTargetIds=694398&694398_type=1&694398_chooseSetUp=-1&694398=1&groupTargetIds=694399&694399_type=1" \
+        "&694399_chooseSetUp=-1&694399=1&groupTargetIds=694400&694400_type=1&694400_chooseSetUp=-1&694400=1" \
+        "&groupTargetIds=694401&694401_type=1&694401_chooseSetUp=-1&694401=1&groupTargetIds=694402&694402_type=2" \
+        "&694402_chooseSetUp=-1&otheranswer_694402=&groupTargetIds=694403&694403_type=2&694403_chooseSetUp=-1" \
+        "&otheranswer_694403=&groupTargetIds=694404&694404_type=4&694404_chooseSetUp=1&jumpInfo=&694404=%E6%97%A0" \
+        "&saveType=2&submitreasons=&submit_highscore_reasons="
     headers['Content-Type'] = "application/x-www-form-urlencoded; charset=UTF-8"
     req = post(
         save_questionnaire_url,
@@ -135,11 +138,13 @@ groupTargetIds=616134&616134_type=4&616134_chooseSetUp=1&jumpInfo=%7B&616134=%E6
 
 
 # cookie默认使用firefox开发者工具中“网路”里请求头中的Cookie格式。即为"key1=value1; key2=value2"
-cookie = input("请在浏览器中登录i.chaoxing.com，然后单击F12，选择“网络”，然后刷新页面，选择base?开头的第一个请求，点击该请求，并在弹出的对话框的“请求头”一栏下找到Cookie: ，右键选择“复制值”，然后粘贴到这里，单击回车\n")
+cookie = input("请在浏览器中登录i.chaoxing.com，然后单击F12，选择“网络”，然后刷新页面，选择base?开头的第一个请求，点击该请求，并在弹出的对话框的“请求头”一栏下找到Cookie: "
+               "，右键选择“复制值”，然后粘贴到这里，单击回车\n")
 system("cls")
 raw_cookie_to_dist()
 sleep(0.5)
 get_questionnaire_list()
+# print(questionnaire_list)
 if questionnaire_remain == 0:
     input("你都填完了啊，没我什么事了，我走了")
     exit()
